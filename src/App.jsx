869b304836c27e631f1b47837b305b16cd37b450
tygris <youssif6920@gmail.com>
2026-01-07 20:59:01 -0700

    threejs setup/test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ThreeScene from "./components/ThreeScene";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+export default function App() {
   return (
     <div>
-      <h1> my portfolio</h1>
+      <div style={{ position: 'absolute', top: 20, left: 20, color: 'white', zIndex: 1 }}>
+        
       </div>
-  )
+      
+      <ThreeScene />
+    </div>
+  );
 }
-
-export default App
